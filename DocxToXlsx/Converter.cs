0678c641c73736f9bb17631c301980e9c45d49e1
Globalcaching <hemenrob@gmail.com>
2015-09-20 12:06:23 +0200

    initial commit

---
+++ DocxToXlsx/Converter.cs
@@ -0,0 +1,176 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using NPOI.XWPF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocxToXlsx
+{
+    public class Converter
+    {
+        private Settings _settings;
+        private List<string> _sourceFiles;
+        private IWorkbook workbook;
+
+        public Converter(Settings settings)
+        {
+            _settings = settings;
+            _sourceFiles = new List<string>();
+        }
+
+        public void Execute()
+        {
+            //get source files
+            if (File.Exists(_settings.ApplicationSettings.FileLocations.Source))
+            {
+                _sourceFiles.Add(_settings.ApplicationSettings.FileLocations.Source);
+            }
+            else if (Directory.Exists(_settings.ApplicationSettings.FileLocations.Source))
+            {
+                GetSourceFiles(_settings.ApplicationSettings.FileLocations.Source);
+            }
+
+            //set destination
+            if (File.Exists(_settings.ApplicationSettings.FileLocations.Destination))
+            {
+                workbook = WorkbookFactory.Create(_settings.ApplicationSettings.FileLocations.Destination);
+            }
+            else
+            {
+                workbook = new XSSFWorkbook();
+            }
+
+            //create sheets
+            foreach (var sheet in _settings.Mapping.Sheets)
+            {
+                if (workbook.GetSheet(sheet.Name) == null)
+                {
+                    workbook.CreateSheet(sheet.Name);
+                }
+            }
+
+            //add captions
+            foreach (var sheet in _settings.Mapping.Sheets)
+            {
+                var wbsheet = workbook.GetSheet(sheet.Name);
+                foreach (var caption in sheet.Elements.Captions)
+                {
+                    ICell cell = wbsheet.GetOrCreateCell(caption.Row, caption.Col);
+                    cell.SetCellValue(caption.Text);
+                }
+            }
+
+            //run through all documents
+            for (int i = 0; i < _sourceFiles.Count; i++)
+            {
+                Console.Write(string.Format("{0}...", Path.GetFileName(_sourceFiles[i])));
+                foreach (var sheet in _settings.Mapping.Sheets)
+                {
+                    var wbsheet = workbook.GetSheet(sheet.Name);
+                    //add filename
+                    foreach (var fn in sheet.Elements.Filenames)
+                    {
+                        ICell cell = wbsheet.GetOrCreateCell(fn.Row, fn.Col);
+                        cell.SetCellValue(Path.GetFileName(_sourceFiles[i]));
+                        fn.SetNextCell();
+                    }
+                    //add paragraph
+                    foreach (var p in sheet.Elements.Paragraphs)
+                    {
+                        ICell cell = wbsheet.GetOrCreateCell(p.Row, p.Col);
+                        cell.SetCellValue(GetParagraphFromDocument(_sourceFiles[i], p.Heading));
+                        p.SetNextCell();
+                    }
+                }
+                Console.WriteLine("gedaan");
+            }
+
+            using (FileStream stream = new FileStream(_settings.ApplicationSettings.FileLocations.Destination, FileMode.Create, FileAccess.Write))
+            {
+                workbook.Write(stream);
+            }
+        }
+
+        /*
+        public string GetParagraphFromDocument(string filename, string displayParagraph)
+        {
+            List<string> headingHirachy = _settings.Mapping.HeadingStyles.ToList();
+            List<string> activeHeading = new List<string>();
+            string activePath = "";
+            StringBuilder sb = new StringBuilder();
+            bool inParagraph = false;
+            using (StreamReader streamReader = new StreamReader(filename))
+            {
+                var doc = new XWPFDocument(streamReader.BaseStream);
+                foreach (var p in doc.Paragraphs)
+                {
+                    int index = headingHirachy.IndexOf(p.Style);
+                    if (index >= 0)
+                    {
+                        if (inParagraph) break;
+                        while (activeHeading.Count > index)
+                        {
+                            activeHeading.RemoveAt(activeHeading.Count - 1);
+                        }
+                        while (activeHeading.Count < index)
+                        {
+                            activeHeading.Add("");
+                        }
+                        activeHeading.Add(p.Text);
+                        activePath = string.Join("@", activeHeading);
+                        sb.Length = 0;
+                    }
+                    else if (string.Compare(displayParagraph,activePath, true)==0)
+                    {
+                        inParagraph = true;
+                        sb.AppendLine(p.Text);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+        */
+
+        public string GetParagraphFromDocument(string filename, string displayParagraph)
+        {
+            List<string> headingHirachy = _settings.Mapping.HeadingStyles.ToList();
+            string activePath = "";
+            StringBuilder sb = new StringBuilder();
+            bool inParagraph = false;
+            using (StreamReader streamReader = new StreamReader(filename))
+            {
+                var doc = new XWPFDocument(streamReader.BaseStream);
+                foreach (var p in doc.Paragraphs)
+                {
+                    int index = headingHirachy.IndexOf(p.Style);
+                    if (index >= 0)
+                    {
+                        if (inParagraph) break;
+                        activePath = p.Text;
+                        sb.Length = 0;
+                    }
+                    else if (string.Compare(displayParagraph, activePath, true) == 0)
+                    {
+                        inParagraph = true;
+                        sb.AppendLine(p.Text);
+                    }
+                }
+            }
+            return sb.ToString();
+        }
+
+        public void GetSourceFiles(string path)
+        {
+            _sourceFiles.AddRange(Directory.GetFiles(path, "*.docx"));
+            var dirs = Directory.GetDirectories(path);
+            foreach (var d in dirs)
+            {
+                GetSourceFiles(d);
+            }
+        }
+    }
+}
---
+++ DocxToXlsx/Settings.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DocxToXlsx
+{
+    [XmlType("settings")]
+    public class Settings
+    {
+        [XmlElement("application")]
+        public ApplicationSettings ApplicationSettings { get; set; }
+
+        [XmlElement("mapping")]
+        public Mapping Mapping { get; set; }
+    }
+
+    public class ApplicationSettings
+    {
+        [XmlElement("locations")]
+        public FileLocations FileLocations { get; set; }
+    }
+
+    public class FileLocations
+    {
+        [XmlElement("source")]
+        public string Source { get; set; }
+
+        [XmlElement("destination")]
+        public string Destination { get; set; }
+    }
+
+    public class Mapping
+    {
+        public Mapping()
+        {
+            HeadingStyles = new List<string>();
+            Sheets = new List<Sheet>();
+        }
+
+        [XmlArray("headingstyles")]
+        [XmlArrayItem("style")]
+        public List<string> HeadingStyles { get; set; }
+
+        [XmlArray("sheets")]
+        [XmlArrayItem("sheet")]
+        public List<Sheet> Sheets { get; set; }
+    }
+
+    public class Sheet
+    {
+        public Sheet()
+        {
+        }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("elements")]
+        public Elements Elements { get; set; }
+    }
+
+    public class Elements
+    {
+        public Elements()
+        {
+            Captions = new List<Caption>();
+            Filenames = new List<Filename>();
+            Paragraphs = new List<Paragraph>();
+        }
+
+        [XmlArray("captions")]
+        [XmlArrayItem("caption")]
+        public List<Caption> Captions { get; set; }
+
+        [XmlArray("filenames")]
+        [XmlArrayItem("filename")]
+        public List<Filename> Filenames { get; set; }
+
+        [XmlArray("paragraphs")]
+        [XmlArrayItem("paragraph")]
+        public List<Paragraph> Paragraphs { get; set; }
+    }
+
+    public class Caption: CellInfo
+    {
+        [XmlElement("text")]
+        public string Text { get; set; }
+    }
+
+    public class Filename: CellInfo
+    {
+    }
+
+    public class Paragraph: CellInfo
+    {
+        [XmlElement("heading")]
+        public string Heading { get; set; }
+    }
+
+
+    public class CellInfo
+    {
+        private static string[] _verticalOrientation = new string[] {"y", "vertical", "row", "rows" };
+        private static string[] _horizontalOrientation = new string[] { "x", "horizontal", "col", "cols" };
+
+        [XmlElement("row")]
+        public int Row { get; set; }
+
+        [XmlElement("col")]
+        public int Col { get; set; }
+
+        [XmlElement("orientation")]
+        public string Orientation { get; set; }
+
+        public void SetNextCell()
+        {
+            if (!string.IsNullOrEmpty(Orientation))
+            {
+                if (_verticalOrientation.Contains(Orientation.ToLower()))
+                {
+                    Row++;
+                }
+                else if (_horizontalOrientation.Contains(Orientation.ToLower()))
+                {
+                    Col++;
+                }
+            }
+        }
+    }
+}
